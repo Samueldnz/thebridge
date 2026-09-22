01a92507ca1c7819a3052addcd2a1f072b2fad5a
Samueldnz <samueldnz03@gmail.com>
2026-09-22 06:49:20 -0300

    fix: import auth module in discovery

--- backend/src/discovery/discovery.module.ts
+++ backend/src/discovery/discovery.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 
+import { AuthModule } from '../auth/auth.module.js';
 import { DiscoveryController } from './discovery.controller.js';
 import { DiscoveryService } from './discovery.service.js';
 
 @Module({
+  imports: [AuthModule],
   controllers: [DiscoveryController],
   providers: [DiscoveryService],
 })
